9488e112d2a5eb38d0187b2c9d81466ca382e63f
Wagner Montes <wmontes@gmail.com>
2026-09-04 19:56:47 -0700

    Scope social preview regression test

--- tests/social-meta.test.js
+++ tests/social-meta.test.js
@@ -2,7 +2,7 @@ import test from 'node:test';
 import assert from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
 
-test('home exposes favicon and social sharing metadata for production domain', async () => {
+test('home exposes favicon and direct raster social sharing metadata for production domain', async () => {
   const html = await readFile(new URL('../index.html', import.meta.url), 'utf8');
   assert.match(html, /rel=["']icon["'][^>]+assets\/favicon\.svg/i);
   assert.match(html, /property=["']og:title["']/i);
@@ -13,6 +13,5 @@ test('home exposes favicon and social sharing metadata for production domain', a
   assert.match(html, /property=["']og:image:width["'][^>]+1200/i);
   assert.match(html, /property=["']og:image:height["'][^>]+630/i);
   assert.match(html, /name=["']twitter:card["'][^>]+summary_large_image/i);
-  assert.match(html, /name=["']twitter:image["'][^>]+\/site-assets\/og\.png/i);
-  assert.doesNotMatch(html, /images\.weserv\.nl|og-consulixo\.svg/i);
+  assert.match(html, /name=["']twitter:image["'][^>]+https:\/\/acjgeiavokkgodhhamak\.supabase\.co\/functions\/v1\/site-assets\/og\.png/i);
 });
